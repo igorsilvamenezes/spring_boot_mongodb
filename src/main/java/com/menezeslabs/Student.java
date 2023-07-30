9bd36a3f2724c4aa87550e536fac29566ae1b1a6
Igor Menezes <igorsilvamenezes@gmail.com>
2023-07-30 01:05:29 -0300

    Created the Index for field email of the Student

--- src/main/java/com/menezeslabs/Student.java
+++ src/main/java/com/menezeslabs/Student.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
@@ -22,6 +23,7 @@ public class Student {
     private String id;
     private String firstName;
     private String lastName;
+    @Indexed(unique = true)
     private String email;
     private Gender gender;
     private Address address;
