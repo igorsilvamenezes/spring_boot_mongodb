b724c40659c988e522f65020b189a031a06e1abc
Igor Menezes <igorsilvamenezes@gmail.com>
2023-07-30 15:48:04 -0300

    query by email to verify student already exists

--- src/main/java/com/menezeslabs/SpringBootMongodbApplication.java
+++ src/main/java/com/menezeslabs/SpringBootMongodbApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -17,7 +20,7 @@ public class SpringBootMongodbApplication {
 	}
 
 	@Bean
-	CommandLineRunner runner(StudentRepository repository) {
+	CommandLineRunner runner(StudentRepository repository, MongoTemplate mongoTemplate) {
 		return args -> {
 			Address address = Address.builder()
 					.country("England")
@@ -25,18 +28,33 @@ public class SpringBootMongodbApplication {
 					.postCode("NE9")
 					.build();
 
+			String email = "jahmed@gmail.com";
 			Student student	= Student.builder()
 					.firstName("Jahmed")
 					.lastName("Ahmed")
-					.email("jahmed@gmail.com")
+					.email(email)
 					.gender(Gender.FEMALE)
 					.address(address)
-					.favouriteSubjects(List.of("Computer Science"))
+					.favouriteSubjects(List.of("Computer Science", "Maths"))
 					.totalSpendInBooks(BigDecimal.TEN)
 					.created(LocalDateTime.now())
 					.build();
 
-			repository.insert(student);
+			Query query = new Query();
+			query.addCriteria(Criteria.where("email").is(email));
+
+			List<Student> students = mongoTemplate.find(query, Student.class);
+
+			if(students.size() > 1) {
+				throw new IllegalStateException("found many students with email " + email);
+			}
+
+			if(students.isEmpty()) {
+				System.out.println("Inserting student " + student);
+				repository.insert(student);
+			} else {
+				System.out.println(student + " already exists");
+			}
 		};
 	}
 }
