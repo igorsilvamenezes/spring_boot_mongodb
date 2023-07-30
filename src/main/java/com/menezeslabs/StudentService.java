bceb88525cd5603c78342470cbe569b41e53f778
Igor Menezes <igorsilvamenezes@gmail.com>
2023-07-30 16:28:05 -0300

    created StudentController and StudentService classes to get all Students

---
+++ src/main/java/com/menezeslabs/StudentService.java
@@ -0,0 +1,17 @@
+package com.menezeslabs;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+}
