c78c30d50bbd8b12b5a2b85cf2953c4196864a65
Igor Menezes <igorsilvamenezes@gmail.com>
2023-07-30 00:35:27 -0300

    Created a CommandRunnerLine for insert a Document into Student Collection

--- src/main/java/com/menezeslabs/SpringBootMongodbApplication.java
+++ src/main/java/com/menezeslabs/SpringBootMongodbApplication.java
@@ -1,7 +1,13 @@
 package com.menezeslabs;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBootMongodbApplication {
@@ -10,4 +16,27 @@ public class SpringBootMongodbApplication {
 		SpringApplication.run(SpringBootMongodbApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(StudentRepository repository) {
+		return args -> {
+			Address address = Address.builder()
+					.country("England")
+	 				.city("London")
+					.postCode("NE9")
+					.build();
+
+			Student student	= Student.builder()
+					.firstName("Jahmed")
+					.lastName("Ahmed")
+					.email("jahmed@gmail.com")
+					.gender(Gender.FEMALE)
+					.address(address)
+					.favouriteSubjects(List.of("Computer Science"))
+					.totalSpendInBooks(BigDecimal.TEN)
+					.created(LocalDateTime.now())
+					.build();
+
+			repository.insert(student);
+		};
+	}
 }
