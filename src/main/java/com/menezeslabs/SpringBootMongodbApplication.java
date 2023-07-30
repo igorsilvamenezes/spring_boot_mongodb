b0738835d3b9a9a54024d25285c6041fa0db77a0
Igor Menezes <igorsilvamenezes@gmail.com>
2023-07-30 16:07:15 -0300

    find student by email using method name in the repository

--- src/main/java/com/menezeslabs/SpringBootMongodbApplication.java
+++ src/main/java/com/menezeslabs/SpringBootMongodbApplication.java
@@ -40,21 +40,33 @@ public class SpringBootMongodbApplication {
 					.created(LocalDateTime.now())
 					.build();
 
-			Query query = new Query();
-			query.addCriteria(Criteria.where("email").is(email));
+			//usingMongoTemplateAndQuery(repository, mongoTemplate, email, student);
 
-			List<Student> students = mongoTemplate.find(query, Student.class);
-
-			if(students.size() > 1) {
-				throw new IllegalStateException("found many students with email " + email);
-			}
-
-			if(students.isEmpty()) {
-				System.out.println("Inserting student " + student);
-				repository.insert(student);
-			} else {
-				System.out.println(student + " already exists");
-			}
+			repository.findStudentByEmail(student.getEmail())
+					.ifPresentOrElse(s -> {
+						System.out.println(student + " already exists");
+					}, () -> {
+						System.out.println("Inserting student " + student);
+						repository.insert(student);
+					});
 		};
+	}
+
+	private void usingMongoTemplateAndQuery(StudentRepository repository, MongoTemplate mongoTemplate, String email, Student student) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("email").is(email));
+
+		List<Student> students = mongoTemplate.find(query, Student.class);
+
+		if(students.size() > 1) {
+			throw new IllegalStateException("found many students with email " + email);
+		}
+
+		if(students.isEmpty()) {
+			System.out.println("Inserting student " + student);
+			repository.insert(student);
+		} else {
+			System.out.println(student + " already exists");
+		}
 	}
 }
